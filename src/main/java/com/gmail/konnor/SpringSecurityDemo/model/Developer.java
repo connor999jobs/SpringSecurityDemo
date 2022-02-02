1bf4e5da8167dbcf6b4f544bce2dd4a506851b84
connor999jobs <coonor999jobs@gmail.com>
2022-02-02 13:37:02 +0200

    Spring Security использувание BcryptPasswordEncoder имя и пароль admin

---
+++ src/main/java/com/gmail/konnor/SpringSecurityDemo/model/Developer.java
@@ -0,0 +1,12 @@
+package com.gmail.konnor.SpringSecurityDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
